0052ea0bca772c1242406cda3ecc400ec81f6112
Anatolii Ruchka <anatoliy.ruchka@gmail.com>
2016-05-16 14:01:16 +0300

    Add pagination for product list

--- kortesMeteorApp/imports/api/products/publish.js
+++ kortesMeteorApp/imports/api/products/publish.js
@@ -1,31 +1,36 @@
 import { Meteor } from 'meteor/meteor';
- 
+import { Counts } from 'meteor/tmeasday:publish-counts';
+
 import { Products } from './collection';
- 
+
 if (Meteor.isServer) {
-  Meteor.publish('products', function() {
+  Meteor.publish('products', function (options) {
     const selector = {
       $or: [{
         // the public products
         $and: [{
           public: true
         }, {
-          public: {
-            $exists: true
-          }
-        }]
+            public: {
+              $exists: true
+            }
+          }]
       }, {
-        // when logged in user is the owner
-        $and: [{
-          owner: this.userId
-        }, {
-          owner: {
-            $exists: true
-          }
+          // when logged in user is the owner
+          $and: [{
+            owner: this.userId
+          }, {
+              owner: {
+                $exists: true
+              }
+            }]
         }]
-      }]
     };
- 
-    return Products.find(selector);
+    //https://github.com/percolatestudio/publish-counts query only  products that should be available to that specific client
+    Counts.publish(this, 'numberOfProducts', Products.find(selector), {
+      noReady: true
+    });
+
+    return Products.find(selector, options);
   });
 }
--- kortesMeteorApp/imports/ui/components/productsList/productsList.js
+++ kortesMeteorApp/imports/ui/components/productsList/productsList.js
@@ -1,9 +1,13 @@
 import * as angular  from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
+import utilsPagination from 'angular-utils-pagination';
+import { Counts } from 'meteor/tmeasday:publish-counts';
+ 
 import { Products } from '../../../api/products/index';
 import { name as ProductAdd } from '../productAdd/productAdd';
 import { name as ProductRemove } from '../productRemove/productRemove';
+
 
 import './productsList.html';
 class ProductsList {
@@ -12,13 +16,33 @@ class ProductsList {
 
         $reactive(this).attach($scope);
 
-        this.subscribe('products');
+        this.perPage = 3;
+        this.page = 1;
+        this.sort = {
+            name: 1
+        };
+
+        this.subscribe('products', () => [{
+            limit: parseInt(this.perPage),
+            skip: parseInt((this.getReactively('page') - 1) * this.perPage),
+            sort: this.getReactively('sort')
+        }
+        ]);
 
         this.helpers({
             products() {
-                return Products.find({});
+                return Products.find({}, {
+                    sort: this.getReactively('sort')
+                });
+            },
+            productsCount() {
+                return Counts.get('numberOfProducts');
             }
         });
+    }
+
+    pageChanged(newPage) {
+        this.page = newPage;
     }
 }
 
@@ -27,7 +51,8 @@ const name = 'productsList';
 // create a module
 export default angular.module(name, [
     angularMeteor,
-    uiRouter
+    uiRouter,
+    utilsPagination
 ]).component(name, {
     templateUrl: `imports/ui/components/${name}/${name}.html`,
     controllerAs: name,
